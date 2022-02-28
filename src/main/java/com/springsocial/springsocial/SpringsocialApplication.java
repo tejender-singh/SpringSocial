5bde3484bd2bcd9b737977eb05668e5afbac731a
Tejender Singh <tejender@outlook.com>
2022-02-28 08:58:26 +0530

    Added base project files
    Added base project files to get started with springboot project

---
+++ src/main/java/com/springsocial/springsocial/SpringsocialApplication.java
@@ -0,0 +1,13 @@
+package com.springsocial.springsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsocial/springsocial/SpringsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsocial.springsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
